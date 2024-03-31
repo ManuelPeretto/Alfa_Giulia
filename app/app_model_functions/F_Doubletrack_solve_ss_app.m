635436224baf47ab4059254228b6e291fba653be
ManuelPeretto <122749898+ManuelPeretto@users.noreply.github.com>
2024-03-31 12:35:43 +0200

    Add files via upload

---
+++ app/app_model_functions/F_Doubletrack_solve_ss_app.m
@@ -0,0 +1,58 @@
+
+%---------------------------------------------------------------
+%  Define Non-Linear sistem
+%---------------------------------------------------------------
+function F = F_Doubletrack_solve_ss(x,U,Vehicle,deltal,deltar,Tyre,choice_model)
+
+toe_r = Vehicle.toe_r;
+
+switch choice_model
+    case 3
+        Fyfl= x(3) * (Tyre.CSf/2);  % Lateral Force Front Left
+        Fyfr= x(4) * (Tyre.CSf/2);  % Lateral Force Front Right
+        Fyrl= x(5) * (Tyre.CSr/2);  % Lateral Force Rear Left
+        Fyrr= x(6) * (Tyre.CSr/2);  % Lateral Force Rear Right
+
+    case 4    
+        ay = U * x(2);           % Lateral accellaration
+        
+        ik= Vehicle.ik;
+        camber_fl = Vehicle.camber_fl(ik);
+        camber_fr = Vehicle.camber_fr(ik);
+        camber_rl = Vehicle.camber_rl(ik);
+        camber_rr = Vehicle.camber_rr(ik);
+
+        Fzfl = Vehicle.m * 9.81 * Vehicle.b / (2*Vehicle.L) + Vehicle.m * ay * Vehicle.h *    Vehicle.d  / (2*Vehicle.Wf);   % Radial Force on front left tyre
+        Fzfr = Vehicle.m * 9.81 * Vehicle.b / (2*Vehicle.L) - Vehicle.m * ay * Vehicle.h *    Vehicle.d  / (2*Vehicle.Wf);   % Radial Force on front right tyre
+        Fzrl = Vehicle.m * 9.81 * Vehicle.a / (2*Vehicle.L) + Vehicle.m * ay * Vehicle.h * (1-Vehicle.d) / (2*Vehicle.Wr);   % Radial Force on rear left tyre
+        Fzrr = Vehicle.m * 9.81 * Vehicle.a / (2*Vehicle.L) - Vehicle.m * ay * Vehicle.h * (1-Vehicle.d) / (2*Vehicle.Wr);   % Radial Force on rear right tyre
+
+        outMF_fl = mfeval(Tyre.Params_f , [Fzfl 0 x(3) -camber_fr 0 U] , 211);
+        outMF_fr = mfeval(Tyre.Params_f , [Fzfr 0 x(4) +camber_fl 0 U] , 211);
+        outMF_rl = mfeval(Tyre.Params_r , [Fzrl 0 x(5) -camber_rr 0 U] , 211);
+        outMF_rr = mfeval(Tyre.Params_r , [Fzrr 0 x(6) +camber_rl 0 U] , 211);
+
+        Fyfl = -outMF_fl(:,2);   % Lateral Force Front Left
+        Fyfr = -outMF_fr(:,2);   % Lateral Force Front Right
+        Fyrl = -outMF_rl(:,2);   % Lateral Force Rear Left
+        Fyrr = -outMF_rr(:,2);   % Lateral Force Rear Right;
+end
+
+switch Vehicle.choice_approx
+    case 1
+        F =[x(3) + ( x(1) + x(2) * Vehicle.a) / (U) - deltal;                            % 0 = alfa_fl + (v + omega*a)/(u + omega*Wf) - deltafrontleft ;
+            x(4) + ( x(1) + x(2) * Vehicle.a) / (U) - deltar;                            % 0 = alfa_fr + (v + omega*a)/(u - omega*Wf) - deltafrontright ;
+            x(5) + ( x(1) - x(2) * Vehicle.b) / (U) - deg2rad(toe_r);                    % 0 = alfa_rl + (v - omega*b)/(u + omega*Wr)
+            x(6) + ( x(1) - x(2) * Vehicle.b) / (U) + deg2rad(toe_r);                    % 0 = alfa_rr + (v - omega*b)/(u - omega*Wr)
+            (Fyfl + Fyfr + Fyrl + Fyrr) / Vehicle.m - x(2) * U;                          % 0 = (Fyfl+Fyfr+Fyrl+Fyrr) / m - omega*u ;
+            (Fyfl + Fyfr) * Vehicle.a - (Fyrl + Fyrr) * Vehicle.b];                      % 0 = ( (Fyfl + Fyfr)*a - (Fyrl + Fyrr)*b ) / J ;
+    case 2
+        F =[x(3) + ( x(1) + x(2) * Vehicle.a) / (U) - deltal;
+            x(4) + ( x(1) + x(2) * Vehicle.a) / (U) - deltar;
+            x(5) + ( x(1) - x(2) * Vehicle.b) / (U) - deg2rad(toe_r);
+            x(6) + ( x(1) - x(2) * Vehicle.b) / (U) + deg2rad(toe_r);
+            (Fyfl*cos(deltal) + Fyfr*cos(deltar) + Fyrl + Fyrr) / Vehicle.m - x(2) * U;
+            (Fyfl*cos(deltal) + Fyfr*cos(deltar))* Vehicle.a - (Fyrl + Fyrr) * Vehicle.b];
+end
+end
+
---
+++ app/app_model_functions/F_Singletrack_ss_app.m
@@ -0,0 +1,87 @@
+
+function [Solution] = F_Singletrack_ss(V,deltaf_vec,Vehicle,Tyre,N,choice_model)
+
+Solution = table;
+alfaf_vec=zeros(N,1);
+alfar_vec=zeros(N,1);
+Fyf_vec=zeros(N,1);
+Fyr_vec=zeros(N,1);
+r_vec=zeros(N,1);
+v_vec=zeros(N,1);
+Solution.exitflag_vec=zeros(N,1);
+mu = zeros(N,1);
+
+tentativo = [ 1.02 , 0.98 , 1.05 , 0.94 ];
+
+x0 = [0.02 0.02 0.1 0.01];   %  initial parameters for [ αF , αR , v , omega ]
+options = optimoptions('fsolve','TolFun',1e-15,'TolX',1e-12,'MaxFunEvals',40000000); % specify options for fsolve : FunctionTolerance , StepTolerance , MaxFunctionEvaluations
+
+for ik=1:N
+    U=V(ik);
+    delta=deg2rad(deltaf_vec(ik));
+            
+    fun = @(x)F_Singletrack_solve_ss(x,U,Vehicle,Tyre,delta,choice_model);
+    
+    [x,fval,exitflag,output] = fsolve(fun,x0,options);                         % solve non linear system
+    
+
+    j=1;
+    while exitflag <= 0 && j <= length(tentativo)
+        x0 = x.*tentativo(j);
+        j=j+1;
+        [x,fval,exitflag,output] = fsolve(fun,x0,options);
+    end
+    
+    if exitflag > 0
+       x0=x;
+    else
+       x0 = [0.02 0.02 0.1 0.01];   
+    end
+
+    Solution.exitflag_vec(ik)=exitflag;
+
+    alfaf_vec(ik)=x(1);                                 % slip angle front vector
+    alfar_vec(ik)=x(2);                                 % slip angle rear vector
+
+    v_vec(ik)=x(3);                                     % lateral speed vector
+    r_vec(ik)=x(4);                                     % yaw rate vector
+    
+    switch choice_model
+        case 1
+            Fyf_vec(ik) = x(1) * Tyre.CSf;
+            Fyr_vec(ik) = x(2) * Tyre.CSr;
+
+        case 2
+
+            inputsMF_f = [Vehicle.Fzf/2 0 x(1) 0 0 U];
+            outMF_f = mfeval(Tyre.Params_f , inputsMF_f , 111);
+
+            inputsMF_r = [Vehicle.Fzr/2 0 x(2) 0 0 U];
+            outMF_r = mfeval(Tyre.Params_r , inputsMF_r , 111);
+
+            Fyf_vec(ik) = -2*outMF_f(:,2);               % lateral force front vector
+            Fyr_vec(ik) = -2*outMF_r(:,2);               % lateral force rear vector
+    end
+
+end
+
+
+
+Solution.u = V';
+Solution.v = v_vec;
+Solution.r = r_vec;
+Solution.r_u = r_vec./V';
+Solution.ay = V' .* r_vec;
+
+Solution.alfa_f=alfaf_vec;
+Solution.alfa_r=alfar_vec;
+
+Solution.Fy_f = Fyf_vec;
+Solution.Fy_r = Fyr_vec;
+
+Solution.deltaf_vec = deltaf_vec';
+Solution.ay_g = Solution.ay./9.81;
+Solution.sterzo_din = rad2deg(deg2rad(Solution.deltaf_vec) - ((Vehicle.L.*Solution.ay)./(Solution.u.^2)));
+Solution.Grad = [(diff(Solution.sterzo_din)./diff(Solution.ay_g));NaN];
+
+end
