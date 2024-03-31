0c1773d6ee2889e45cbc5357925c2cc4eb644883
ManuelPeretto <122749898+ManuelPeretto@users.noreply.github.com>
2024-03-31 12:28:29 +0200

    Add files via upload

--- Script Giulia/prove/prova_ack.m
+++ Script Giulia/prove/prova_ack.m
@@ -2,26 +2,33 @@
 addpath('C:\Users\manue\Desktop\Script\Functions')
 
 toe=0;
-Wf=1.5;
-L=3.11;
+Wf=1.6;
+L=2.82;
 
-deltaf=0.145;
+deltaf=6.55; % [deg]
 
 percentuali=[-1:0.1:1];
 
 deltal=zeros(1,length(percentuali));
 deltar=zeros(1,length(percentuali));
 
-for ii=1:length(percentuali);
-    [deltal(ii),deltar(ii)]=Ackermann(deltaf,toe,percentuali(ii),Wf,L);
+for ii=1:length(percentuali)
+    [deltal(ii),deltar(ii)]=Ackermann(deg2rad(deltaf),toe,percentuali(ii),Wf,L);
 
 end
 
 figure
 hold on
-plot(percentuali,deltal);
-plot(percentuali,deltar);
-yline(deltaf,'--g')
-legend('deltal','deltar');
-xlabel('ackermann percent');
-ylabel('\delta [rad]');
+grid on
+set(gca,'TickLabelInterpreter','latex');
+plot(percentuali*100,rad2deg(deltal),linewidth=1.5,color='blue');
+plot(percentuali*100,rad2deg(deltar),linewidth=1.5,color='red');
+yline(deltaf,'--g',LineWidth=1.5)
+%yline(max((rad2deg(deltal))),'--b');
+%yline(min((rad2deg(deltar))),'--b');
+plot(100,deltaf,'xr','MarkerSize',15,LineWidth=1.5)
+plot(100,max((rad2deg(deltar))),'xr','MarkerSize',15,LineWidth=1.5);
+plot(100,min((rad2deg(deltal))),'xr','MarkerSize',15,LineWidth=1.5);
+legend('$\delta_{out}$','$\delta_{in}$',Interpreter='latex',fontsize=16);
+xlabel('Ackermann %');
+ylabel('$\delta [deg]$',Interpreter='latex',fontsize=14);
--- Script Giulia/prove/prova_ack_2.m
+++ Script Giulia/prove/prova_ack_2.m
@@ -5,29 +5,30 @@ clc
 addpath("C:\Users\manue\Desktop\Script Giulia\Functions")
 
 
-Wf=1.5;
-L=3.11;
+Vehicle.Wf=1.5;
+Vehicle.L=3.11;
 
 deltaf_vec=linspace(0,0.2,100);
 
-toe=deg2rad(0); % toe-in (positivo) [ruote convergono]
-percentuale=1;
+Vehicle.toe_f=deg2rad(0); % toe-in (positivo) [ruote convergono]
+Vehicle.percentuale_Ack=1;
 
-txt = [' $Ackermann = ',num2str(percentuale*100),'\%$'];
+txt = [' $Ackermann = ',num2str(Vehicle.percentuale_Ack*100),'\%$'];
 
 
-[deltal,deltar]=Ackermann(deltaf_vec,toe,percentuale,Wf,L);
+[deltal,deltar]=Ackermann(deltaf_vec,Vehicle);
 
 
 figure
 hold on
+grid on
 set(gca,'TickLabelInterpreter','latex');
 subtitle(txt,Interpreter='latex',fontsize=14);
 plot(rad2deg(deltaf_vec),rad2deg(deltal),linewidth=1.5,color='blue');
 plot(rad2deg(deltaf_vec),rad2deg(deltar),linewidth=1.5,color='red');
 %yline(0,'--g');
-yline(rad2deg(toe),'--b');
-yline(rad2deg(-toe),'--b');
+yline(rad2deg(Vehicle.toe_f),'--b');
+yline(rad2deg(-Vehicle.toe_f),'--b');
 %xline(0,'--y');
 %xline(rad2deg(toe),'--g');
 legend('$\delta_{out}$','$\delta_{in}$','',Interpreter='latex',fontsize=16);
